28e3504e8cd538f8a1ac03b2de988b10656869e9
Soapfoot <soapfoot@hotmail.com>
2014-05-15 19:12:02 -0400

    it's working. hurray! all i need to do now is to add auto updating and run it in the background

--- reader.py
+++ reader.py
@@ -1,10 +1,19 @@
 import urllib2, subprocess
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from contextlib import closing
 
 post_class_name = 'postMessage' #'postContainer replyContainer'
 
 def parse_post(post):
+    for line in post:
+        if line.strip() == '<br/>':
+            continue
+
+            # <br/> parsed out
+            # <s> </s> mark spoiler tags
+            # remember to parse out the &gt; at the beginning of every quote block
+            # quotelink classes can be identified with last 4 numbers? just put them in their own context for now,
+            # deal with them later
     pass
 
 
@@ -16,30 +25,38 @@ def get_posts(url):
         soup = BeautifulSoup(page.read())
         posts = soup.find_all(class_=post_class_name)
         for post in posts:
-            print '\n----------------------\n', post.prettify()
-            parsed = parse_post(post)
-            parsed_posts.append(parsed)
-            # need to ignore quotelinks, and put quotes in their own special greentext context. maybe keep tuples,
-            # with one as the content, and the other as content type, then dispatch text to speech on content type.
-            # parsing the html itself shouldn't be too tough, unless you want to get fancy with contexts.
-            # go line by line through the prettification, throw away all of the <br/>'s, and maybe make spoilers their
-            # own thing too. that's about all that you can do i think. spoilers, greentexts, and regular text.
-            # can also have a quote context so we can tell if it's a reply to someone.
+            #print '\n--- Post ---\n'#, post.prettify(), '\n'
 
-            # <br/> parsed out
-            # <s> </s> mark spoiler tags
-            # remember to parse out the &gt; at the beginning of every quote block
-            # quotelink classes can be identified with last 4 numbers? just put them in their own context for now,
-            # deal with them later
-
-            # text to speech bit:
-            # say -f mynovel.txt -o myaudiobook.aiff
-            # probably don't want to do txt files though, b/c we're planning on switching speaker tones and stuff.
-
+            for child in post.children:
+                if isinstance(child, Tag):
+                    if child.text.strip() == '':
+                        continue
+                    child_class = None
+                    if child.get('class') is not None:
+                        child_class = child.get('class')[0]
+                    if child_class is not None:
+                        if child_class == 'quotelink':
+                            print 'Quotelink: ', child, '\ntext = ', child.text
+                            parsed = (child.text, 'quotelink')
+                        elif child_class == 'quote':
+                            print 'Greentext: ', child, '\nText = ', child.text
+                            parsed = (child.text, 'greentext')
+                    elif '<s>' in str(child) and '</s>' in str(child):
+                        print 'Spoiler: ', child.text
+                        parsed = (child.text, 'spoiler')
+                    else:
+                        print 'Mystery text:', child
+                        parsed = (child.text, 'mystery')
+                else:
+                    print 'Plain text:', child
+                    parsed = (child, 'normal')
+                parsed_posts.append(parsed)
     return parsed_posts
 
 
 # vocalize a [(text, context)] array based on the context
+# need to insert blank spots, or add waiting between posts.
+# often they flow together uncomfortably.
 def vocalize_in_context(voices):
     for text,context in voices:
         if context == 'normal':
@@ -50,7 +67,10 @@ def vocalize_in_context(voices):
 
         elif context == 'greentext':
             speaker = 'Alex' # better. i like how he sounds more aggressive. fits well
-
+            text = text[1:] # omit the '>' todo: add text stripping to all of these
+        elif context == 'quotelink':
+            speaker = 'Kathy'
+            text = 'at' + text[-4:] # only say the last 4 digits
         else:
             speaker = 'Zarvox'
 
@@ -64,9 +84,18 @@ def say(text, voice):
 def main():
     # while thread has not 404'rd, crank posts.
     # say('Yolo live once negroes','Pipe Organ')
-    vocalize_in_context([('implying i will read all of that fucking text', 'greentext')])#, ("pants sluts for ever uguu~", 'spoiler'),
+    #vocalize_in_context([('implying i will read all of that fucking text', 'greentext')])#, ("pants sluts for ever uguu~", 'spoiler'),
         #('wearing a fedora tunic in winter', 'greentext')])
-    #get_posts('https://boards.4chan.org/co/thread/61850685/rip-detective-quentin-the-cross-lance')
+    posts = get_posts('https://boards.4chan.org/co/thread/61850685/rip-detective-quentin-the-cross-lance')
+
+    vocalize_in_context(posts)
+
+    # todo: group everything into a post.
+    for text, mode in posts:
+        print '--- --- ---'
+        print 'Mode = ', mode
+        print 'Text = ', text
+
 
 if __name__ == '__main__':
     main()
